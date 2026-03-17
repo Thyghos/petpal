4fff5c010c4e4ed583fbed2e0a91ad7c276daad8
John Alecci <ealecci@gmail.com>
2026-03-17 13:47:06 -0700

    Initial Commit

---
+++ PetpalTests/PetpalTests.swift
@@ -0,0 +1,16 @@
+//
+//  PetpalTests.swift
+//  PetpalTests
+//
+//  Created by John Alecci on 3/17/26.
+//
+
+import Testing
+
+struct PetpalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
